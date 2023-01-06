ab9340aa1a7f600e4252bbc386aab75c36a0cabf
Alexander Pietsch <alexander.pietsch@diva-e.com>
2023-01-06 13:52:05 +0100

    playing around
    Signed-off-by: Alexander Pietsch <alexander.pietsch@diva-e.com>

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from Server');
+  res.end('Hello my friend');
   console.log('serving response ..')
 });
 
